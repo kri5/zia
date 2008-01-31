e3599e4da51c29fd5ffe6eafb7237c24bb67bbd4
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2008-01-31 18:05:51 +0000

    One file missing... my bad. refs #9

---
+++ src/IClientSocket.h
@@ -0,0 +1,14 @@
+#ifndef __ICLIENT_SOCKET_H__
+#define __ICLIENT_SOCKET_H__
+
+/// Interface for ClientSocket classes, to ensure multi-platform compactibility
+class IClientSocket
+{
+public:
+	/// Will receive data from client.
+	virtual int	recv(char *buf, int length) const = 0;
+	/// Will send datas to client.
+	virtual int send(char *buf, int length) const = 0;
+};
+
+#endif //__ICLIENT_SOCKET_H__
