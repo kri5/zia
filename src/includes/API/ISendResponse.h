1e221b8a34aa7123c5e5694e7bd53a4af5012d61
Thomas Boquet <thomas.boquet@gmail.com>
2008-04-03 18:14:49 +0000

    Pretty much all the API interfaces are now documented. There's still some undone work in the ISendResponse cos we still have to make a decision on how the data soure module will be passed

--- src/includes/API/ISendResponse.h
+++ src/includes/API/ISendResponse.h
@@ -7,11 +7,30 @@
 
 #include <fstream>
 
+/**
+ *  This hook interface forces you to implements for event methods.
+ *  <ul>
+ *      <li>The first one (setInput) will be called to construct the module chain</li>
+ *      <li>The second one (onPreSend) will be called before send any of the content data</li>
+ *      <li>The third one (onProcessContent) will be called n times for processing the content to send</li>
+ *      <li>The forth one (onPostSend) will be called after the entire content has been sent</li>
+ *  </ul>
+ */
 /// Base interface for modules who wants to hook on response sending events.
 class ISendResponse : public IModule
 {
     public:
+
+        /**
+         *  The interface virtual destructor
+         */
         virtual ~ISendResponse() {};
+
+        /**
+         *  The setInput event.
+         *  This method is kind of special because it aims to build a chain of module.
+         *  on progress
+         */
         virtual void    setInput(IModule*) = 0;
 
         virtual ChainStatus onPreSend(IHttpRequest*, IHttpResponse*) = 0;
--- src/includes/API/IWorkflow.h
+++ src/includes/API/IWorkflow.h
@@ -5,13 +5,50 @@
 #include "API/IHttpRequest.h"
 #include "API/IHttpResponse.h"
 
+/**
+ *  This hook interfaces forces you to implement tree event methods.<br>
+ *  <ul>
+ *      <li>The first one (onBegin) will be called at the begining of a transaction</li>
+ *      <li>The second one (onEnd) will be called at the end of the transaction</li>
+ *      <li>The third one (onFailure) will be called if somethinf failed during the transaction</li>
+ *  </ul>
+ */
 /// Base interface for modules who wants to hook on main workflow events.
 class IWorkflow : public IModule
 {
     public:
+        /**
+         *  The interface virtual destructor
+         */
         virtual ~IWorkflow() {};
-        virtual ChainStatus     onBegin(IHttpRequest*, IHttpResponse*) = 0;
-        virtual ChainStatus     onEnd(IHttpRequest*, IHttpResponse*) = 0;
+
+        /**
+         *  The begin event.<br>
+         *  This method will be called at the begining of the transaction 
+         *  (after the accept event of the INetwork hook interface).
+         *  @param request the IHttpRequest of the transaction
+         *  @param response the IHttpResponse of the transaction
+         *  @return a ChainStatus
+         */
+        virtual ChainStatus     onBegin(IHttpRequest* request, IHttpResponse* response) = 0;
+
+        /**
+         *  The en event.<br>
+         *  This method will be called at the end of the transaction
+         *  (when all the data has been sent and just before the end of the task)
+         *  @param request the IHttpRequest of the transaction
+         *  @param response the IHttpResponse of the transaction
+         *  @return a ChainStatus
+         */
+        virtual ChainStatus     onEnd(IHttpRequest* request, IHttpResponse* response) = 0;
+
+        /**
+         *  The failure event.<br>
+         *  This method will be called when a module failed to do his job and returned an ERRORMODULE ChainStatus.
+         *  @param request the IHttpRequest of the transaction
+         *  @param response the IHttpResponse of the transaction
+         *  @return a ChainStatus
+         */
         virtual ChainStatus     onFailure(IHttpRequest*, IHttpResponse*) = 0;
 
 };
