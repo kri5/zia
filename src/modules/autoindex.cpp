5503b1d087a2c93dd0f41b693e9c995b206d7fea
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2008-04-24 13:14:55 +0000

    Autoindex know cares about the config. fixes #85

--- src/modules/autoindex.cpp
+++ src/modules/autoindex.cpp
@@ -34,33 +34,51 @@ int         AutoIndex::getPriority(zAPI::IModule::Event event) const
 
 zAPI::IModule::ChainStatus    AutoIndex::onPreBuild(zAPI::IHttpRequest* request, zAPI::IHttpResponse* response)
 {
+    if (request->getConfig()->isSet("AutoIndex") == false)
+        return IModule::CONTINUE;
+    const std::string& autoIndex = *(request->getConfig()->getParam("AutoIndex"));
     std::string fileName = request->getConfig()->getParamChar("DocumentRoot") + request->getUri();
     struct stat st;
+
     if (stat(fileName.c_str(), &st) < 0)
         return IModule::CONTINUE;
     if (S_ISDIR(st.st_mode))
     {
-        DIR*			dir = opendir(fileName.c_str());
-        struct dirent*	file;
+        size_t	begin = 0;
+        size_t	end;
+        std::string     indexToSearch;
+        while ((end = autoIndex.find(' ', begin)) != std::string::npos)
+        {
+            indexToSearch = autoIndex.substr(begin, end - begin);
+            DIR*			dir = opendir(fileName.c_str());
+            struct dirent*	file;
 
-        if (!dir)
-        {
-            closedir(dir);
-            return IModule::CONTINUE;
-        }
-        while ((file = readdir(dir)))
-        {
-            if (file->d_name[0] == '.' &&
-                    (file->d_name[1] == 0 || (file->d_name[1] == '.' && file->d_name[2] == 0)))
-                continue ;
-            if (strcmp(file->d_name, "index.php") == 0)
-            {
-                request->setUri(request->getUri() + "/index.php");
-                closedir(dir);
+            if (!dir)
                 return IModule::CONTINUE;
+            while ((file = readdir(dir)))
+            {
+                if (file->d_name[0] == '.' &&
+                        (file->d_name[1] == 0 || (file->d_name[1] == '.' && file->d_name[2] == 0)))
+                    continue ;
+                {
+                    if (indexToSearch == file->d_name)
+                    {
+                        request->setUri(request->getUri() + indexToSearch);
+                        closedir(dir);
+                        return IModule::CONTINUE;
+                    }
+                }
+                indexToSearch = autoIndex.substr(begin, end - begin);
+                if (indexToSearch == file->d_name)
+                {
+                    request->setUri(request->getUri() + indexToSearch);
+                    closedir(dir);
+                    return IModule::CONTINUE;
+                }
             }
+            begin = end + 1;
+            closedir(dir);
         }
-        closedir(dir);
     }
     return IModule::CONTINUE;
 }
