1bb0ea49019890d7a915dfa570911806fbc9edf9
Christophe Courtaut <christophe.courtaut@gmail.com>
2013-09-11 23:56:28 +0200

    Refactor of Socket

--- src/ClientSocket_unix.cpp
+++ src/ClientSocket_unix.cpp
@@ -11,99 +11,92 @@
 #include "MemoryManager.hpp"
 
 int ClientSocket::_nbSockets = 0;
-Mutex   ClientSocket::_mutex;
+Mutex ClientSocket::_mutex;
 
-ClientSocket::ClientSocket(int acceptedSocket, const std::string& address, int port) : _closed(false), _address(address), _port(port)
-{
-	listenSocket = acceptedSocket;
-    struct timeval tv;
-    tv.tv_sec = 1;
-    tv.tv_usec = 0;
-    setsockopt(listenSocket, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(struct timeval));
-    ClientSocket::_nbSockets++;
+ClientSocket::ClientSocket(int acceptedSocket, const std::string &address,
+                           int port)
+    : _closed(false), _address(address), _port(port) {
+  listenSocket = acceptedSocket;
+  struct timeval tv;
+  tv.tv_sec = 1;
+  tv.tv_usec = 0;
+  setsockopt(listenSocket, SOL_SOCKET, SO_RCVTIMEO, &tv,
+             sizeof(struct timeval));
+  ClientSocket::_nbSockets++;
 }
 
-ClientSocket::~ClientSocket()
-{
-	this->close(true);
-    MutexLock   getLock(ClientSocket::_mutex);
-    --ClientSocket::_nbSockets;
+ClientSocket::~ClientSocket() {
+  this->close(true);
+  MutexLock getLock(ClientSocket::_mutex);
+  --ClientSocket::_nbSockets;
 }
 
-int             ClientSocket::send(const char *buf, int length)
-{
-    ModuleManager::getInstance().call(zAPI::IModule::NetworkHook, zAPI::IModule::onSendEvent, buf, length, &zAPI::INetwork::onSend);
-    int iResult = ::send(listenSocket, buf, length, MSG_NOSIGNAL);
+int ClientSocket::send(const char *buf, int length) {
+  ModuleManager::getInstance().call(zAPI::IModule::NetworkHook,
+                                    zAPI::IModule::onSendEvent, buf, length,
+                                    &zAPI::INetwork::onSend);
+  int iResult = ::send(listenSocket, buf, length, MSG_NOSIGNAL);
 
-	if (iResult == SOCKET_ERROR)
-        Logger::getInstance() << Logger::NoStdOut << Logger::Error << "Send error : " << strerror(errno) << Logger::Flush;
-	return (iResult);
+  if (iResult == SOCKET_ERROR)
+    Logger::getInstance() << Logger::NoStdOut << Logger::Error
+                          << "Send error : " << strerror(errno)
+                          << Logger::Flush;
+  return (iResult);
 }
 
-int             ClientSocket::send(const std::string& buf, int length)
-{
-    std::cout << "WARNING" <<std::endl;
-    std::string tmp = buf;
-    int ret;
-    int remainingSize = length;
-    if (length == -1)
-        remainingSize = tmp.size();
+int ClientSocket::send(const std::string &buf, int length) {
+  std::cout << "WARNING" << std::endl;
+  std::string tmp = buf;
+  int ret;
+  int remainingSize = length;
+  if (length == -1)
+    remainingSize = tmp.size();
 
-    while (remainingSize > 0)
-    {
-        ret = send(tmp.c_str(), remainingSize);
-        if (ret == -1) return -1;
-        tmp.erase(0, ret);
-        remainingSize -= ret;
-    }
-    return 0;
+  while (remainingSize > 0) {
+    ret = send(tmp.c_str(), remainingSize);
+    if (ret == -1)
+      return -1;
+    tmp.erase(0, ret);
+    remainingSize -= ret;
+  }
+  return 0;
 }
 
-int             ClientSocket::recv(char *buf, int length)
-{
-	int iResult = ::recv(listenSocket, buf, length, 0);
-	if (iResult == SOCKET_ERROR)
-        Logger::getInstance() << Logger::NoStdOut << Logger::Error << "Receive error : " << strerror(errno) << Logger::Flush;
-    else
-        ModuleManager::getInstance().call(zAPI::IModule::NetworkHook, zAPI::IModule::onReceiveEvent, buf, iResult, &zAPI::INetwork::onReceive);
-	return (iResult);
+int ClientSocket::recv(char *buf, int length) {
+  int iResult = ::recv(listenSocket, buf, length, 0);
+  if (iResult == SOCKET_ERROR)
+    Logger::getInstance() << Logger::NoStdOut << Logger::Error
+                          << "Receive error : " << strerror(errno)
+                          << Logger::Flush;
+  else
+    ModuleManager::getInstance().call(
+        zAPI::IModule::NetworkHook, zAPI::IModule::onReceiveEvent, buf, iResult,
+        &zAPI::INetwork::onReceive);
+  return (iResult);
 }
 
-int             ClientSocket::getNativeSocket() const
-{
-    return listenSocket;
+int ClientSocket::getNativeSocket() const { return listenSocket; }
+
+int ClientSocket::countSockets() { return ClientSocket::_nbSockets; }
+
+bool ClientSocket::isClosed() const { return this->_closed; }
+
+void ClientSocket::close(bool shutdown) {
+  if (this->_closed == false) {
+    if (shutdown)
+      if (::shutdown(listenSocket, SHUT_RDWR))
+        Logger::getInstance() << Logger::NoStdOut << Logger::Error
+                              << "Can't shutdown socket : " << strerror(errno)
+                              << Logger::Flush;
+    if (::close(listenSocket) < 0)
+      Logger::getInstance() << Logger::NoStdOut << Logger::Error
+                            << "Can't close socket : " << strerror(errno)
+                            << Logger::Flush;
+    this->_closed = true;
+  }
 }
 
-int             ClientSocket::countSockets()
-{
-    return ClientSocket::_nbSockets;
-}
+const std::string &ClientSocket::getIP() const { return this->_address; }
 
-bool            ClientSocket::isClosed() const
-{
-    return this->_closed;
-}
-
-void            ClientSocket::close(bool shutdown)
-{
-    if (this->_closed == false)
-    {
-        if (shutdown)
-            if (::shutdown(listenSocket, SHUT_RDWR))
-                Logger::getInstance() << Logger::NoStdOut << Logger::Error << "Can't shutdown socket : " << strerror(errno) << Logger::Flush;
-        if (::close(listenSocket) < 0)
-            Logger::getInstance() << Logger::NoStdOut << Logger::Error << "Can't close socket : " << strerror(errno) << Logger::Flush;
-        this->_closed = true;
-    }
-}
-
-const std::string&  ClientSocket::getIP() const
-{
-    return this->_address;
-}
-
-int                 ClientSocket::getPort() const
-{
-    return this->_port;
-}
+int ClientSocket::getPort() const { return this->_port; }
 
--- src/includes/API/IClientSocket.h
+++ src/includes/API/IClientSocket.h
@@ -9,80 +9,79 @@ typedef int SOCKET;
 #include <windows.h>
 #endif
 
-namespace zAPI
-{
-    /// Interface for hooking socket creation, after accept()
-    class IClientSocket
-    {
-        public:
+namespace zAPI {
+/// Interface for hooking socket creation, after accept()
+class IClientSocket {
+public:
 
-            /**
-             *  The interface virtual destructor
-             */
-            virtual ~IClientSocket(){}
+  /**
+   *  The interface virtual destructor
+   */
+  virtual ~IClientSocket() {}
 
-            /**
-             *  Returns the size that has been read on the socket and
-             *  fills the buffer passed in parameter with the received data
-             *  that will never been greater than the specified length.
-             *  @param buf a buffer
-             *  @param length the maximum length to read
-             *  @return the read length
-             */
-            virtual int	    recv(char *buf, int length) = 0;
+  /**
+   *  Returns the size that has been read on the socket and
+   *  fills the buffer passed in parameter with the received data
+   *  that will never been greater than the specified length.
+   *  @param buf a buffer
+   *  @param length the maximum length to read
+   *  @return the read length
+   */
+  virtual int recv(char *buf, int length) = 0;
 
-            /**
-             *  Sends a certain amount of data contained in the specified 
-             *  buffer (as a char*) through the socket.
-             *  @param buf the buffer to send
-             *  @param length the wanted size to send
-             *  @return the size sent through the socket
-             */
-            virtual int     send(const char *buf, int length) = 0;
+  /**
+   *  Sends a certain amount of data contained in the specified
+   *  buffer (as a char*) through the socket.
+   *  @param buf the buffer to send
+   *  @param length the wanted size to send
+   *  @return the size sent through the socket
+   */
+  virtual int send(const char *buf, int length) = 0;
 
-            /**
-             *  Sends a certain amount of data contained in the specified 
-             *  buffer (as a std::string) through the socket.
-             *  @param buf the buffer to send
-             *  @param length wanted size to send (or -1, if you want to send the entire string)
-             *  @return the size sent through the socket
-             */
-            virtual int     send(const std::string& buf, int length) = 0;
+  /**
+   *  Sends a certain amount of data contained in the specified
+   *  buffer (as a std::string) through the socket.
+   *  @param buf the buffer to send
+   *  @param length wanted size to send (or -1, if you want to send the entire
+   * string)
+   *  @return the size sent through the socket
+   */
+  virtual int send(const std::string &buf, int length) = 0;
 
-            /**
-             *  Close the socket, and eventually shut it down nicelly.
-             *
-             *  @param niceClosing If true, close will call shutdown(2)
-             */
-            virtual void    close(bool niceClosing) = 0;
+  /**
+   *  Close the socket, and eventually shut it down nicelly.
+   *
+   *  @param niceClosing If true, close will call shutdown(2)
+   */
+  virtual void close(bool niceClosing) = 0;
 
-            /**
-             *  Check if socket has already be closed, to prevent multiple socket
-             *  closure.
-             *
-             *  @return a boolean indicating if socket has been closed.
-             */
+  /**
+   *  Check if socket has already be closed, to prevent multiple socket
+   *  closure.
+   *
+   *  @return a boolean indicating if socket has been closed.
+   */
+  virtual bool isClosed() const = 0;
 
-            virtual bool    isClosed() const = 0;
-            /**
-             * Get the underlying socket used for the client
-             * @return a socket identifier
-             */
-            virtual SOCKET  getNativeSocket() const = 0;
+  /**
+   * Get the underlying socket used for the client
+   * @return a socket identifier
+   */
+  virtual SOCKET getNativeSocket() const = 0;
 
-            /**
-             *  Get the client's IP address
-             *  @return the client's IP
-             */
-            virtual const std::string&  getIP() const = 0;
+  /**
+   *  Get the client's IP address
+   *  @return the client's IP
+   */
+  virtual const std::string &getIP() const = 0;
 
-            /**
-             *  Get the client connection port
-             *  @return the port used by the client to connect to the server
-             */
-            virtual int                 getPort() const = 0;
+  /**
+   *  Get the client connection port
+   *  @return the port used by the client to connect to the server
+   */
+  virtual int getPort() const = 0;
 
-    };
+};
 }
 
 #endif //ICLIENT_SOCKET_H__
