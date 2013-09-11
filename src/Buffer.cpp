05a4451e9a87a7da2019cab6dd54c5df7a6ce3f1
Christophe Courtaut <christophe.courtaut@gmail.com>
2013-09-11 23:52:21 +0200

    Refactor of Buffer

--- src/Buffer.cpp
+++ src/Buffer.cpp
@@ -6,312 +6,157 @@
 
 #include "MemoryManager.hpp"
 
-Buffer::Buffer(size_t capacity) : _capacity(capacity), _eol(-1), _bufPos(0)
-{
-    _size = 0;
+Buffer::Buffer(size_t capacity)
+    : _capacity(capacity), _eol(-1), _bufPos(0), _size(0) {}
+
+Buffer::~Buffer() {
+  for (auto buffer : this->_buffers)
+    delete buffer;
+  this->_buffers.clear();
 }
 
-Buffer::~Buffer()
-{
-    std::list<std::string*>::iterator      it = this->_buffers.begin();
-    std::list<std::string*>::iterator      ite = this->_buffers.end();
+bool Buffer::hasEOL() const { return this->_eol >= 0; }
 
-    while (it != ite)
-    {
-        delete *it;
-        ++it;
+size_t Buffer::capacity() const { return this->_capacity; }
+
+size_t Buffer::size() const { return this->_size; }
+
+void Buffer::push(const char *data, size_t size) {
+  this->_size += size;
+  if (this->_buffers.size() == 0 ||
+      this->_buffers.back()->length() + size > this->_capacity) {
+    std::string *str = new std::string(data, size);
+    this->_buffers.push_back(str);
+    this->_bufPos = 0;
+  } else {
+    this->_buffers.back()->append(data, size);
+    this->_bufPos += size;
+  }
+  if (this->hasEOL() == false)
+    this->getEolPos();
+}
+
+void Buffer::pushFront(const std::string &buff) {
+  this->_size += buff.size();
+  this->_buffers.push_front(new std::string(buff));
+  this->getEolPos();
+}
+
+void Buffer::packBuffer(size_t begin) {
+  if (this->_buffers.size() == 0) {
+    return;
+  }
+  //std::cout << "last erase caracter : " << this->_buffers.front()->at(begin)
+  //<< std::endl;
+  this->_buffers.front()->erase(0, begin);
+  this->_bufPos = 0;
+  this->getEolPos();
+}
+
+void Buffer::get(std::string &res, size_t length) {
+  //size_t                                  i;
+  size_t nb = 0;
+  auto it = this->_buffers.begin();
+  auto ite = this->_buffers.end();
+
+  res.clear();
+  res.reserve(length);
+  while (nb < length && it != ite) {
+    if ((*it)->length() >= length) {
+      res.append(*(*it), 0, length);
+      nb += length;
+      break;
+    } else {
+      res.append(*(*it));
+      length -= (*it)->length();
+      nb += (*it)->length();
     }
-    this->_buffers.clear();
+    ++it;
+  }
+  this->_readCount = nb;
 }
 
-bool    Buffer::hasEOL() const
-{
-    return this->_eol >= 0;
-}
+char Buffer::getChar(size_t pos) {
+  size_t i = 0;
 
-size_t  Buffer::capacity() const
-{
-    return this->_capacity;
-}
-
-size_t  Buffer::size() const
-{
-    return this->_size;
-}
-
-void    Buffer::push(const char* data, size_t size)
-{
-    this->_size += size;
-    if (this->_buffers.size() == 0 || 
-            this->_buffers.back()->length() + size > this->_capacity)
-    {
-        std::string* str = new std::string(data, size);
-        this->_buffers.push_back(str);
-        this->_bufPos = 0;
-    }
+  for (auto buffer : this->_buffers) {
+    if (pos >= i + buffer->length())
+      i += buffer->length();
     else
-    {
-        this->_buffers.back()->append(data, size);
-        this->_bufPos += size;
+      return buffer->at(pos - i);
+  }
+  return -1;
+}
+
+void Buffer::dump() {
+  for (auto buffer : this->_buffers)
+    std::cout << buffer << "]><[";
+  std::cout << std::endl;
+}
+
+void Buffer::getEolPos() {
+  size_t pos;
+
+  for (auto buffer : this->_buffers) {
+   if ((pos = buffer->find("\r\n")) != std::string::npos) {
+     this->_eol = pos;
+     return;
+   }
+  }
+  this->_eol = -1;
+}
+
+bool Buffer::getLine(std::string &res) {
+  if (this->hasEOL()) {
+    this->get(res, this->_eol + 2);
+    return true;
+  }
+  return false;
+}
+
+void Buffer::flush(size_t length) {
+  if (length == 0)
+    return;
+  auto it = this->_buffers.begin();
+  auto end = this->_buffers.end();
+  size_t nb;
+  size_t i;
+  size_t strLength;
+
+  for (nb = 0; it != end && length > 0;) {
+    strLength = (*it)->length();
+    for (i = 0; i < strLength; ++i, ++nb) {
+      if (i == strLength) {
+        break;
+      }
+      if (nb == length) {
+        this->packBuffer(i);
+        this->_size -= i;
+        return;
+      }
     }
-    if (this->hasEOL() == false)
-        this->getEolPos();
+    delete *it;
+    it = this->_buffers.erase(it);
+    this->_size -= strLength;
+    if (nb == length - 1)
+      return;
+  }
+  this->_size = 0;
 }
 
-void    Buffer::pushFront(const std::string& buff)
-{
-    this->_size += buff.size();
-    this->_buffers.push_front(new std::string(buff));
-    this->getEolPos();
+void Buffer::flush() { this->flush(this->gcount()); }
+
+void Buffer::clear() {
+  for (auto buffer : this->_buffers)
+	  delete buffer;
+  this->_size = 0;
+  this->_buffers.clear();
+  this->_bufPos = 0;
+  this->_eol = -1;
+  _buffers.push_back(new std::string);
 }
 
-void    Buffer::packBuffer(size_t begin)
-{
-    if (this->_buffers.size() == 0)
-    {
-        return ;
-    }
-    //std::cout << "last erase caracter : " << this->_buffers.front()->at(begin) << std::endl;
-    this->_buffers.front()->erase(0, begin);
-    this->_bufPos = 0;
-    this->getEolPos();
-}
+size_t Buffer::gcount() const { return this->_readCount; }
 
-void	Buffer::get(std::string& res, size_t length)
-{
-	//size_t                                  i;
-	size_t                                  nb = 0;
-	std::list<std::string*>::iterator       it = this->_buffers.begin();
-	std::list<std::string*>::iterator       ite = this->_buffers.end();
-
-	//i = 0;
-	//for (nb = 0; nb < length; ++nb)
-	//{
-	//	if (i == (*it)->length())
-	//	{
-	//		++it;
-	//		if (it == end)
-	//		{
-	//			this->_readCount = nb;
-	//			res[nb] = 0;
-	//			return ;
-	//		}
-	//		i = 0;
-	//	}
-	//	else if (nb == this->_size)
-	//	{
-	//		this->_readCount = nb;
-	//		res[nb] = 0;
-	//		return ;
-	//	}
-	//	res[nb] = (*it)->at(i);
-	//	++i;
-	//}
-	//res[nb] = 0;
-	//this->_readCount = nb;
-    res.clear(); 
- 	res.reserve(length);
-    while (nb < length && it != ite) 
-    { 
-        if ((*it)->length() >= length) 
-        { 
-            res.append(*(*it), 0, length); 
-            nb += length; 
-            break ; 
-        } 
-        else 
-        { 
-            res.append(*(*it)); 
-            length -= (*it)->length(); 
-            nb += (*it)->length(); 
-        } 
-        ++it; 
-    } 
-    this->_readCount = nb;
-}
-
-//char*   Buffer::get(size_t length)
-//{
-//    char* res = new char[length + 1];
-//	this->get(res, length);
-//	return res;
-//}
-
-char    Buffer::getChar(size_t pos)
-{
-	size_t                                  i = 0;
-    std::list<std::string*>::iterator       it = this->_buffers.begin();
-    std::list<std::string*>::iterator       end = this->_buffers.end();
-    
-    if (it == end)
-        return -1;
-    for (; it != end; ++it)
-    {
-        if (pos >= i + (*it)->length()) //resultat dans un buffer suivant.
-        {
-            i += (*it)->length();
-            continue ;
-        }
-        else //le resultat est dans ce buffer.
-        {
-            if (pos >= i + (*it)->length())
-            {
-                //std::cout << "pos == " << pos << std::endl << " i == " << i << " length " << (*it)->length() << std::endl;
-                //this->dump();
-                return -1;
-            }
-            return ((*it)->at(pos - i));
-        }
-    }
-    return -1;
-}
-
-void    Buffer::dump()
-{
-    std::list<std::string*>::iterator       it;
-
-    for (it = this->_buffers.begin(); it != this->_buffers.end(); ++it)
-        std::cout << *(*it) << "]><[";
-    std::cout << std::endl;
-}
-
-void        Buffer::getEolPos()
-{
-    std::list<std::string*>::iterator       it = this->_buffers.begin();
-    std::list<std::string*>::iterator       ite = this->_buffers.end();
-
-    size_t      pos; 
-    for (; it != ite; ++it) 
-    { 
-        if ((pos = (*it)->find("\r\n")) != std::string::npos) 
-        { 
-            this->_eol = pos; 
-            return ; 
-        } 
-    } 
-    this->_eol = -1; 
-    //std::list<std::string*>::iterator       it = this->_buffers.begin();
-    //std::list<std::string*>::iterator       end = this->_buffers.end();
-    //int                                     nb;
-    //size_t                                  i;
-    //bool                                    backslashAire = false;
-    //size_t                                  length;
-
-    //for (nb = 0; it != end; ++it)
-    //{
-    //    length = (*it)->length();
-    //    for (i = 0; i < length; ++i, ++nb)
-    //    {
-    //        if (backslashAire == true)
-    //        {
-    //            if ((*it)->at(i) == '\n')
-    //            {
-    //                this->_eol = nb;
-    //                return ;    
-    //            }
-    //            else
-    //                backslashAire = false;
-    //        }
-    //        else
-    //        {
-    //            if ((*it)->at(i) == '\r')
-    //            {
-    //                backslashAire = true;
-    //            }
-    //        } 
-    //    }
-    //}
-    //this->_eol = -1;
-}
-
-bool    Buffer::getLine(std::string& res)
-{
-    if (this->hasEOL())
-    {
-        this->get(res, this->_eol + 2);
-        return true;
-    }
-    return false;
-}
-
-void    Buffer::flush(size_t length)
-{
-    if (length == 0)
-        return ;
-    std::list<std::string*>::iterator       it = this->_buffers.begin();
-    std::list<std::string*>::iterator       end = this->_buffers.end();
-    size_t                                  nb;
-    size_t                                  i;
-    size_t                                  strLength;
-
-    for (nb = 0; it != end && length > 0; )
-    {
-        strLength = (*it)->length();
-        for (i = 0; i < strLength; ++i, ++nb)
-        {
-            if (i == strLength) //warning : bug generator
-            {
-                break ;
-            }
-            if (nb == length)
-            {
-                this->packBuffer(i);
-                this->_size -= i;
-                return ;
-            }
-        }
-        delete *it;
-        it = this->_buffers.erase(it);
-        this->_size -= strLength;
-        if (nb == length - 1)
-            return ;
-        //if (length >= strLength)
-        //{
-        //    delete *it;
-        //    it = this->_buffers.erase(it);
-        //    length -= strLength;
-        //    this->_size -= strLength;
-        //    continue ;
-        //}
-        //else
-        //{
-        //    this->packBuffer(length);
-        //    this->_size -= strLength;
-        //    return ;
-        //}
-    }
-    this->_size = 0;
-}
-
-void    Buffer::flush()
-{
-    this->flush(this->gcount());
-}
-
-void    Buffer::clear()
-{
-    std::list<std::string*>::iterator      it = this->_buffers.begin();
-    std::list<std::string*>::iterator      end = this->_buffers.end();
-
-    while (it != end)
-    {
-        delete *it;
-        ++it;
-    }
-    this->_size = 0;
-    this->_buffers.clear();
-    this->_bufPos = 0;
-    this->_eol = -1;
-    _buffers.push_back(new std::string);
-}
-
-size_t  Buffer::gcount() const
-{
-    return this->_readCount;
-}
-
-bool    Buffer::empty() const
-{
-    return (this->_size == 0);
-}
+bool Buffer::empty() const { return (this->_size == 0); }
 
--- src/includes/Utils/Buffer.h
+++ src/includes/Utils/Buffer.h
@@ -5,37 +5,37 @@
 #include <cstdlib>
 #include <string>
 
-class Buffer
-{
-    public:
-        Buffer(size_t capacity);
-        ~Buffer();
+class Buffer {
+public:
+  Buffer(size_t capacity);
+  ~Buffer();
 
-        bool        hasEOL() const;
-	/// Buff needs to be length + 1 char.
-	void 	    get(std::string& buff, size_t length);
-        void        push(const char* data, size_t size);
-        void        pushFront(const std::string& data);
-        size_t      capacity() const;
-        size_t      size() const;
-        bool        getLine(std::string&); 
-        void        dump();
-        void        flush(size_t);
-        void        flush();
-        void        clear();
-        size_t      gcount() const;
-        bool        empty() const;
-        char        getChar(size_t);
-    private:
-        size_t                  _capacity;
-        size_t                  _size;
-        int                     _eol;
-        size_t                  _bufPos;
-        std::list<std::string*> _buffers;
-        size_t                  _readCount;
+  bool hasEOL() const;
+  /// Buff needs to be length + 1 char.
+  void get(std::string &buff, size_t length);
+  void push(const char *data, size_t size);
+  void pushFront(const std::string &data);
+  size_t capacity() const;
+  size_t size() const;
+  bool getLine(std::string &);
+  void dump();
+  void flush(size_t);
+  void flush();
+  void clear();
+  size_t gcount() const;
+  bool empty() const;
+  char getChar(size_t);
 
-        void                    packBuffer(size_t);
-        void                    getEolPos();
+private:
+  size_t _capacity;
+  size_t _size;
+  int _eol;
+  size_t _bufPos;
+  std::list<std::string *> _buffers;
+  size_t _readCount;
+
+  void packBuffer(size_t);
+  void getEolPos();
 };
 
 #endif //BUFFER_H__
