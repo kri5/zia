0e08498cf7ae438a4bb07fc8562800f7fa66e8e7
Christophe Courtaut <christophe.courtaut@gmail.com>
2013-09-12 00:10:59 +0200

    Reformating of ErrorMsg

--- src/ErrorMsg.cpp
+++ src/ErrorMsg.cpp
@@ -11,56 +11,34 @@
 #include "Workflow/Pool.h"
 #include "Stream/ResponseStreamSocket.h"
 
-const char* ClientSocket::Error::Msg[] = {
-    "Unknown error",
-    "Send error",
-    "Receive error",
-    "Disconnect error"
+const char *ClientSocket::Error::Msg[] = { "Unknown error", "Send error",
+                                           "Receive error",
+                                           "Disconnect error" };
+
+const char *IMainSocket::Error::Msg[] = { "Unknown error",
+                                          "Can't create socket",
+                                          "Can't bind socket", "Listen error",
+                                          "Accept error" };
+
+const char *NetworkID::Error::Msg[] = { "Unknown error", "Invalid config",
+                                        "Port can't be wildcarded" };
+
+const char *Vhost::Error::Msg[] = {
+  "Unknown error", "Asked for an invalid paramter from a Virtual Host"
 };
 
-const char* IMainSocket::Error::Msg[] = {
-	"Unknown error",
-	"Can't create socket",
-	"Can't bind socket",
-	"Listen error",
-	"Accept error"
-};
+const char *Port::Error::Msg[] = { "Unknown error", "Given port is invalid" };
 
-const char*	NetworkID::Error::Msg[] = {
-	"Unknown error",
-	"Invalid config",
-	"Port can't be wildcarded"
-};
+const char *Server::Error::Msg[] = { "Unknown error", "Can't bind socket",
+                                     "Select error" };
 
-const char* Vhost::Error::Msg[] = {
-	"Unknown error",
-	"Asked for an invalid paramter from a Virtual Host"
-};
+const char *Config::Error::Msg[] = { "Unknown error",
+                                     "Can't access this out of root config" };
 
-const char*	Port::Error::Msg[] = {
-	"Unknown error",
-	"Given port is invalid"
-};
+const char *Pool::Manager::Error::Msg[] = { "Unknown error", "Poll() error" };
 
-const char* Server::Error::Msg[] = {
-	"Unknown error",
-    "Can't bind socket",
-	"Select error"
-};
-
-const char*	Config::Error::Msg[] = {
-	"Unknown error",
-	"Can't access this out of root config"
-};
-
-const char* Pool::Manager::Error::Msg[] = {
-    "Unknown error",
-    "Poll() error"
-};
-
-const char* ResponseStreamSocket::Error::Msg[] = {
-    "Unknown error",
-    "Can't ask for a socket stream size"
+const char *ResponseStreamSocket::Error::Msg[] = {
+  "Unknown error", "Can't ask for a socket stream size"
 };
 
 // Archi dependent part.
@@ -73,23 +51,18 @@ const char* ResponseStreamSocket::Error::Msg[] = {
 
 #include "File/File_win32.h"
 
-const char* File::Error::Msg[] = {
-	"Unknown error",
-	"Can't get file's attributes"
-};
+const char *File::Error::Msg[] = { "Unknown error",
+                                   "Can't get file's attributes" };
 
 #include "FileSystem/FileSystem_win32.h"
 
-const char* FileSystem::Error::Msg[] = {
-	"Unknown error",
-	"Can't have valid file handle"
-};
+const char *FileSystem::Error::Msg[] = { "Unknown error",
+                                         "Can't have valid file handle" };
 
 #include "Time/Time_win32.h"
 
-const char* Time::Error::Msg[] = {
-	"Unknown error",
-	"Can't convert from FILETIME to SYSTEMTIME"
+const char *Time::Error::Msg[] = {
+  "Unknown error", "Can't convert from FILETIME to SYSTEMTIME"
 };
 
 ////////////////////////////////////////////////////////////
@@ -102,16 +75,12 @@ const char* Time::Error::Msg[] = {
 
 #else
 
-const char* File::Error::Msg[] = {
-	"Unknown error",
-	"No such file or directory"
-};
+const char *File::Error::Msg[] = { "Unknown error",
+                                   "No such file or directory" };
 
-const char* FileSystem::Error::Msg[] = {
-	"Unknown error",
-	"Can't opendir for reading",
-	"Can't read directory with readdir",
-	"Can't stat file"
-};
+const char *FileSystem::Error::Msg[] = { "Unknown error",
+                                         "Can't opendir for reading",
+                                         "Can't read directory with readdir",
+                                         "Can't stat file" };
 #endif
 
