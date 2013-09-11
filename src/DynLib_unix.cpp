573a278e474e8bd4df83753d0ce25844fc706df2
Christophe Courtaut <christophe.courtaut@gmail.com>
2013-09-12 00:09:54 +0200

    Refactor of DynLib

--- src/DynLib_unix.cpp
+++ src/DynLib_unix.cpp
@@ -1,57 +1,48 @@
 #include "Modules/DynLib.h"
 #include <dlfcn.h>
 
-DynLib::DynLib() : _err(NULL)
-{
+DynLib::DynLib() : _err(NULL) {}
 
+DynLib::~DynLib() {
+  if (this->_err != NULL)
+    delete this->_err;
+  close();
 }
 
-DynLib::~DynLib()
-{
+bool DynLib::load(const std::string &filename) {
+  if ((this->handle = dlopen(filename.c_str(), RTLD_LAZY)) == 0)
+    return false;
+  return true;
+}
+
+void *DynLib::sym(const std::string &symbol) {
+  void *ret;
+  char *err;
+
+  dlerror();
+  if (this->_err != NULL)
+    delete this->_err;
+  ret = dlsym(this->handle, symbol.c_str());
+  err = dlerror();
+  if (err != NULL)
+    this->_err = new std::string(err);
+  return ret;
+}
+
+void DynLib::close() {
+  dlclose(this->handle);
+}
+
+const char *DynLib::lastError() {
+  char *err;
+
+  err = dlerror();
+  if (err != NULL) {
     if (this->_err != NULL)
-        delete this->_err;
-	close();
-}
-
-bool		DynLib::load(const std::string& filename)
-{
-	if ((this->handle = dlopen(filename.c_str(), RTLD_LAZY)) == 0)
-        return false;
-    return true;
-}
-
-void*		DynLib::sym(const std::string& symbol)
-{
-    void*   ret;
-    char*   err;
-
-    dlerror();
-    if (this->_err != NULL)
-        delete this->_err;
-    ret = dlsym(this->handle, symbol.c_str());
-    err = dlerror();
-    if (err != NULL)
-        this->_err = new std::string(err);
-	return ret;
-}
-
-void		DynLib::close()
-{
-	dlclose(this->handle);
-}
-
-const char*       DynLib::lastError()
-{
-    char*   err;
-
-    err = dlerror();
-    if (err != NULL)
-    {
-        if (this->_err != NULL)
-            delete this->_err;
-        this->_err = new std::string(err);
-    }
-    if (this->_err != NULL)
-        return this->_err->c_str();
-    return NULL;
+      delete this->_err;
+    this->_err = new std::string(err);
+  }
+  if (this->_err != NULL)
+    return this->_err->c_str();
+  return NULL;
 }
--- src/includes/Modules/DynLib_unix.h
+++ src/includes/Modules/DynLib_unix.h
@@ -4,19 +4,18 @@
 #include <string>
 #include "Modules/IDynLib.h"
 
-class DynLib : public IDynLib
-{
+class DynLib : public IDynLib {
 public:
-    DynLib();
-	~DynLib();
-	bool		    load(const std::string& filename);
-	void*		    sym(const std::string& symbol);
-	void		    close();
-    const char*     lastError();
+  DynLib();
+  ~DynLib();
+  bool load(const std::string &filename);
+  void *sym(const std::string &symbol);
+  void close();
+  const char *lastError();
 
 private:
-	void*		    handle;
-    std::string*    _err;
+  void *handle;
+  std::string *_err;
 };
 
 #endif // DYNLIB_UNIX_H
